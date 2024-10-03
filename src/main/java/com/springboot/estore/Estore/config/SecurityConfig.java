fe9d2067a9f23ee179558c91145b4ece50e410af
pranav033 <pranjalpranav0708@gmail.com>
2024-10-03 13:56:57 +0530

    Changed from beasic security to jwt security in SecurityConfig class.

--- src/main/java/com/springboot/estore/Estore/config/SecurityConfig.java
+++ src/main/java/com/springboot/estore/Estore/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.springboot.estore.Estore.config;
 
 
+import com.springboot.estore.Estore.security.JwtAuthenticationEntryPoint;
+import com.springboot.estore.Estore.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -9,15 +12,23 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity(debug = true)
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter filter;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint entryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -42,7 +53,17 @@ public class SecurityConfig {
         );
 
         //type of security
-        httpSecurity.httpBasic(Customizer.withDefaults());
+//        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        //entry point
+        httpSecurity.exceptionHandling(ex-> ex.authenticationEntryPoint(entryPoint));
+
+        //session creation policy
+        httpSecurity.sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // our filer works before the spring filter
+        httpSecurity.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+
         return httpSecurity.build();
     }
 
