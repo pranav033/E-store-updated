87a1bba93e3fd68d05af34e374a1f2d07e07e056
pranav033 <pranjalpranav0708@gmail.com>
2024-10-08 16:14:26 +0530

    Added login with google button in the frontend.

--- frontend/google-login-app/src/app/app.component.ts
+++ frontend/google-login-app/src/app/app.component.ts
@@ -1,13 +1,22 @@
+import { GoogleSigninButtonModule, SocialAuthService, SocialLoginModule } from '@abacritt/angularx-social-login';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SocialLoginModule, GoogleSigninButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'google-login-app';
+
+  constructor(private authService : SocialAuthService){
+      this.authService.authState.subscribe((authData)=>{
+        console.log("User logged in :");
+        console.log(authData);
+        console.log("ID Token --> "+authData.idToken)
+      })
+  }
 }
--- frontend/google-login-app/src/app/app.config.ts
+++ frontend/google-login-app/src/app/app.config.ts
@@ -2,7 +2,28 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { FacebookLoginProvider, GoogleLoginProvider, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        lang: 'en',
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '122967120813-ct57cgrpupnpbp60nu13pon19db0v29m.apps.googleusercontent.com'
+            )
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+  ]
 };
